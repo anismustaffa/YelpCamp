d13dfcf15b117345b03c1751ef4f4b687287bf02
Anis Mustaffa <97100561+anismustaffa@users.noreply.github.com>
2023-09-11 00:29:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ const styleSrcUrls = [
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
     "https://cdn.jsdelivr.net",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
