4f53d161207608a41c197f33d6333b4fbd41cbec
Anis Mustaffa <97100561+anismustaffa@users.noreply.github.com>
2023-09-11 00:34:54 -0700

    Update app.js
    delete unnecessary link

--- app.js
+++ app.js
@@ -82,7 +82,6 @@ const styleSrcUrls = [
     "https://fonts.googleapis.com/",
     "https://use.fontawesome.com/",
     "https://cdn.jsdelivr.net",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
